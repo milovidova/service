49bcc504b6d45d2ca6f25fafef7266b597175434
milovidova <145771799+milovidova@users.noreply.github.com>
2025-10-02 17:26:08 +0300

    Init

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
+gem "carrierwave"
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -61,3 +63,4 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,104 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
+
+# posts = [
+#   {
+# 	title: "The Bad Seeds",
+# 	body: "Tracklist",
+# 	author: "Nick Cave"
+#   },
+#   {
+# 	title: "The Bad Seeds Old",
+# 	body: "Some Old Tracklist",
+# 	author: "Nick Cave Old Too"
+#   }
+# ]
+
+# Post.destroy_all
+# puts "All posts just destroyed"
+
+# posts.each do |post|
+#   p = Post.create!(post)
+#   puts "Some magic just created a post with title #{ p.title } and with id #{ p.id }!"
+# end
+
+
+
+
+# @raw_text = 'Дом Наркомфина — один из знаковых памятников архитектуры советского авангарда и конструктивизма. Построен в 1928—1930 годах по проекту архитекторов Моисея Гинзбурга, Игнатия Милиниса и инженера Сергея Прохорова для работников Народного комиссариата финансов СССР (Наркомфина). Автор замысла дома Наркомфина Гинзбург определял его как «опытный дом переходного типа». Дом находится в Москве по адресу: Новинский бульвар, дом 25, корпус 1. С начала 1990-х годов дом находился в аварийном состоянии, был трижды включён в список «100 главных зданий мира, которым грозит уничтожение». В 2017—2020 годах отреставрирован по проекту АБ «Гинзбург Архитектс», функционирует как элитный жилой дом. Отдельно стоящий «Коммунальный блок» (историческое название) планируется как место проведения публичных мероприятий.'
+# @words = @raw_text.downcase.gsub(/[—.—,«»:()]/, '').gsub(/  /, ' ').split(' ')
+
+# def create_sentence
+#   sentence_words = []
+
+#   (10..20).to_a.sample.times do
+#     sentence_words << @words.sample
 #   end
+
+#   sentence = sentence_words.join(' ').capitalize + '.'
+# end
+
+# 100.times do
+#   post = Post.create!(title: create_sentence, body: create_sentence)
+#   puts "Post with id #{post.id} just created"
+# end
+
+# posts = Post.all
+
+# posts.each do |post|
+#   (2..8).to_a.sample.times do
+#     comment = post.comments.create!(body: create_sentence)
+#     puts "Comment with id #{comment.id} for post with id #{comment.post.id} just created"
+#   end
+# end
+
+
+
+
+@raw_text = 'Дом Наркомфина — один из знаковых памятников архитектуры советского авангарда и конструктивизма. Построен в 1928—1930 годах по проекту архитекторов Моисея Гинзбурга, Игнатия Милиниса и инженера Сергея Прохорова для работников Народного комиссариата финансов СССР (Наркомфина). Автор замысла дома Наркомфина Гинзбург определял его как «опытный дом переходного типа». Дом находится в Москве по адресу: Новинский бульвар, дом 25, корпус 1. С начала 1990-х годов дом находился в аварийном состоянии, был трижды включён в список «100 главных зданий мира, которым грозит уничтожение». В 2017—2020 годах отреставрирован по проекту АБ «Гинзбург Архитектс», функционирует как элитный жилой дом. Отдельно стоящий «Коммунальный блок» (историческое название) планируется как место проведения публичных мероприятий.'
+@words = @raw_text.downcase.gsub(/[—.—,«»:()]/, '').gsub(/  /, ' ').split(' ')
+
+def seed
+  reset_db
+  create_posts(10)
+  create_comments(2..8)
+end
+
+def reset_db
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+end
+
+def create_sentence
+  sentence_words = []
+
+  (10..20).to_a.sample.times do
+    sentence_words << @words.sample
+  end
+
+  sentence = sentence_words.join(' ').capitalize + '.'
+end
+
+def upload_random_image
+  uploader = PostCoverUploader.new(Post.new, :cover)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/post_covers', '*')).sample))
+  uploader
+end
+
+def create_posts(quantity)
+  quantity.times do
+    post = Post.create!(title: create_sentence, body: create_sentence, cover: upload_random_image)
+    puts "Post with id #{post.id} just created"
+  end
+end
+
+def create_comments(quantity)
+  Post.all.each do |post|
+    quantity.to_a.sample.times do
+      comment = post.comments.create!(body: create_sentence)
+      puts "Comment with id #{comment.id} for post with id #{comment.post.id} just created"
+    end
+  end
+end
+
+seed
+
